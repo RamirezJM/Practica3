32e94d2a992963e43b796c6d4c0bd6bb1da21553
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-07-09 23:55:25 -0400

    renderize products function

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -64,13 +64,18 @@ const productos = [
 ];
 
 let carrito = []
-let galeriaProductos = document.querySelector('.productos-galeria')
+
+const carroCompras = document.querySelector('.carro-compras')
+const contenedorProductosCarro = document.querySelector('.contenedor-productos-carro')
+const totalCarro = document.querySelector('.total-carro')
+const cuentaProductos = document.querySelector('.total-productos-carro')
+const galeriaProductos = document.querySelector('.productos-galeria')
 galeriaProductos.innerHTML = ''
 
-const tarjetaProducto = productos.forEach( producto => {   
-   const productoHtml =
- `<div class="card">
-    <img src=${producto.imagen} class="card-img-top" alt="imagen">
+const tarjetaProducto = productos.forEach(producto => {
+  const productoHtml =
+    `<div class="card">
+    <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
     <div class="card-body">
       <h5 class="card-title">${producto.nombre}</h5>
       <p class="card-text-precio">$${producto.precio}</p>
@@ -81,35 +86,74 @@ const tarjetaProducto = productos.forEach( producto => {
   galeriaProductos.insertAdjacentHTML("beforeend", productoHtml)
 })
 
-  galeriaProductos.addEventListener('click', (event) => {
-     const botonAgregar = event.target.closest('.btn-agregar')
-     if(botonAgregar){
-     agregarProducto(botonAgregar)
-     }
- })   
+galeriaProductos.addEventListener('click', (event) => {
+  const botonAgregar = event.target.closest('.btn-agregar')
+  if (botonAgregar) {
+    agregarProducto(botonAgregar)
+  }
+})
 
-  function agregarProducto(botonAgregar){
-      const productoId = botonAgregar.id
-      
-      const productoAlCarro = productos.find(producto => producto.id === productoId)
+const iconoCarro = document.querySelector('header>.fa-cart-shopping')
+iconoCarro.addEventListener('click', () => {
+  carroCompras.classList.add('carro-activo')
+})
 
-      const productoExistente = carrito.find(prod => prod.id === productoId)
-      if(productoExistente){
-        productoExistente.quantity++
-      }
-      else{
-        carrito.push({...productoAlCarro, quantity: 1})
-        console.log(`${productoAlCarro.nombre} añadido al carro`)
-      }
-     renderizarCarrito()
-     guardarCarrito()
-    }
+const cerrarCarro = document.querySelector('.fa-x')
+cerrarCarro.addEventListener('click', () => {
+  carroCompras.classList.remove('carro-activo')
+})
 
-function renderizarCarrito(){
+//  AGREGAR PRODUCTO 
+
+function agregarProducto(botonAgregar) {
+  const productoId = botonAgregar.id
+
+  const productoAlCarro = productos.find(producto => producto.id === productoId)
+
+  const productoExistente = carrito.find(prod => prod.id === productoId)
+  if (productoExistente) {
+    productoExistente.cantidad++
+  }
+  else {
+    carrito.push({ ...productoAlCarro, cantidad: 1 })
+    console.log(`${productoAlCarro.nombre} añadido al carro`)
+  }
+  renderizarCarrito()
+  guardarCarrito()
+}
+
+//  RENDERIZAR PRODUCTO EN CARRO
+
+
+function renderizarCarrito() {
+  contenedorProductosCarro.innerHTML = ''
+
+  let totalCompra = 0
+  let unidadesEnCarro = 0
+
+  carrito.forEach(producto => {
+    const productoSubTotal = producto.precio * producto.cantidad
+    totalCompra += productoSubTotal
+    unidadesEnCarro += producto.cantidad
+
+    const productoCarroHtml = `
+                            <div class="producto-carro" data-id = "${producto.id}">
+                              <h4>${producto.nombre}</h4>
+                              <img src="${producto.imagen}" alt="${producto.nombre}">
+                              <p>$${producto.precio.toLocaleString('es-CL')}</p>
+                              <input type="number" value="${producto.cantidad}" min="1" data-id="${producto.id}">
+                          
+                              <button class="btn-eliminar" data-id="${producto.id}">X</button>
+                            </div>
+                            `
+    contenedorProductosCarro.insertAdjacentHTML('beforeend', productoCarroHtml)
+  })
+
+
 
 }
 
-function guardarCarrito(){
+function guardarCarrito() {
 
 }
 
